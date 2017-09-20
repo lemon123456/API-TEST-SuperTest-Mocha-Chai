1e17ba02e5e01c41c084f602927466c4561de706
Xiaojuan Xu <xjxu@thoughtworks.com>
2017-09-20 11:39:26 +0800

    Add an Express middleware

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -1,8 +1,8 @@
 'use strict';
+
 module.exports = function(app) {
     var todoList = require('../controllers/todoListController');
 
-    // todoList Routes
     app.route('/tasks')
         .get(todoList.list_all_tasks)
         .post(todoList.create_a_task);
