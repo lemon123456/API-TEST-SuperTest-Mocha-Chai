deb5af2edc39b721fa2add41f209a689bf7cdb82
Xiaojuan Xu <xjxu@thoughtworks.com>
2017-09-21 17:48:11 +0800

    Create api test

--- test/todos.test.js
+++ test/todos.test.js
@@ -7,15 +7,39 @@ var app = require("../server"),
 var expect = chai.expect;
 
 describe('Todos list api integration test', function(){
-    describe('#get / tasks', function () {
+    var task = {
+        name: 'integration test'
+    };
+
+    describe('#get /tasks', function () {
        it('should get all tasks', function(done){
-           request(app).get('/tasks')
-               .end(function (err, res) {
+           request(app).get('/tasks').end(function (err, res) {
                    expect(res.statusCode).to.equal(200);
                    expect(res.body).to.be.an('array');
-                   expect(res.body).to.have.lengthOf(3);
+                   // expect(res.body).to.have.lengthOf(4);
                    done();
                });
+       });
+    });
+
+    describe('# create /tasks', function(){
+        it('should create a task', function(done){
+            request(app).post('/tasks').send(task).end(function (err, res) {
+               expect(res.statusCode).to.equal(200);
+               expect(res.body.name).to.equal('integration test');
+               task = res.body;
+               done();
+            });
+        });
+    });
+
+    describe('#get /taks by id', function () {
+       it('should get a task', function(done){
+           request(app).get('/tasks/' + task._id).end(function(err, res){
+             expect(res.statusCode).to.equal(200);
+             expect(res.body.name).to.equal('integration test');
+             done();
+           });
        });
     });
 });
