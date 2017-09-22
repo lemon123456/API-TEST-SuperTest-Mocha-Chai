9c4e56ec55555dfb37ed702a5cbd9d10f3fe78be
Xiaojuan Xu <xjxu@thoughtworks.com>
2017-09-22 11:04:05 +0800

    Refact test file

--- test/todos.test.js
+++ test/todos.test.js
@@ -6,41 +6,73 @@ var app = require("../server"),
 
 var expect = chai.expect;
 
-describe('Todos list api integration test', function(){
+describe('Todos list api integration test', function () {
     var task = {
         name: 'integration test'
     };
 
-    describe('#get /tasks', function () {
-       it('should get all tasks', function(done){
-           request(app).get('/tasks').end(function (err, res) {
+    describe('Create a task', function () {
+        it('should create a task', function (done) {
+            request(app)
+                .post('/tasks')
+                .send(task)
+                .end(function (err, res) {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.body.name).to.equal('integration test');
+                    task = res.body;
+                    done();
+                });
+        });
+    });
+
+    describe('Get all tasks', function () {
+        it('should get all tasks', function (done) {
+            request(app)
+                .get('/tasks')
+                .end(function (err, res) {
+                    expect(res.statusCode).to.be.equal(200);
+                    expect(res.body).to.be.an('array');
+                    done();
+                });
+        });
+    });
+
+    describe('Get a task by id', function () {
+        it('should get a task', function (done) {
+            request(app)
+                .get('/tasks/' + task._id)
+                .end(function (err, res) {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.body.name).to.equal('integration test');
+                    done();
+                });
+        });
+    });
+
+    describe('Update a task by id', function () {
+       it('should modify a task', function(done){
+           task.name = 'New Task';
+           request(app)
+               .put('/tasks/' + task._id)
+               .send(task)
+               .end(function (err, res) {
                    expect(res.statusCode).to.equal(200);
-                   expect(res.body).to.be.an('array');
-                   // expect(res.body).to.have.lengthOf(4);
+                   expect(res.body.name).to.equal('New Task');
                    done();
                });
        });
     });
 
-    describe('# create /tasks', function(){
-        it('should create a task', function(done){
-            request(app).post('/tasks').send(task).end(function (err, res) {
-               expect(res.statusCode).to.equal(200);
-               expect(res.body.name).to.equal('integration test');
-               task = res.body;
-               done();
-            });
+    describe('Delete a task by id', function(){
+        it('should delete a task', function(done){
+            request(app)
+                .delete('/tasks/' + task._id)
+                .end(function(err, res){
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.body.message).to.equal('Task successfully deleted');
+                    done();
+                });
         });
-    });
-
-    describe('#get /taks by id', function () {
-       it('should get a task', function(done){
-           request(app).get('/tasks/' + task._id).end(function(err, res){
-             expect(res.statusCode).to.equal(200);
-             expect(res.body.name).to.equal('integration test');
-             done();
-           });
-       });
     });
 });
 
